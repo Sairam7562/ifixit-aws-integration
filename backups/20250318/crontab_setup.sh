4d5e3d26768cd65447148e082f9af6b61c0cf964
EC2 Default User <ec2-user@ip-10-0-7-143.ec2.internal>
2025-03-24 00:27:16 +0000

    Ignore backups folder

--- backups/20250318/crontab_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Create cron job for daily updates at 2 AM
-(crontab -l 2>/dev/null; echo "0 2 * * * cd $(pwd) && python3 ifixit_fetcher.py >> logs/fetcher_\$(date +\%Y\%m\%d).log 2>&1") | crontab -
-echo "Crontab has been set up for daily updates at 2 AM"
